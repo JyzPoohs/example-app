944201be761b4acaeabbd3d06a96f5356a5b5cea
JyzPoohs <jessie1695@gmail.com>
2023-04-02 23:42:16 +0800

    yeah 3

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,26 @@ Route::get('/index', function () {
 //With parameter
 Route::get('/users/{id}', function ($id) {
     echo $id;
-});
+})->where('id', '[0-9]+');
 
 //Optional parameter
 Route::get('/user/{name?}', function (string $name = "Jane") {
     return $name;
-});
+})->where('name', '[A-Za-z]+');
+
+//Multiple restrictions
+Route::get('/user/{id}/{name}', function (string $id, string $name) {
+    return 'Name: ' . $name . ' ID: ' . $id;
+})->where(['id' => '[0-9]+', 'name' => '[a-z]+']);
+
+//Naming route
+// Route::get('/userss/{id}/profile', function (string $id) {
+//     echo $id;
+// })->name('profile');
+
+// $url = route('profile', ['id' => 1]);
+
+//Controller Route
+use App\Http\Controllers\UserController;
+
+Route::get('/user/{id}', [UserController::class, 'show']);
